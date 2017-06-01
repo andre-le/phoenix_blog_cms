7f87b69160834e370ae9f36c8b8fb2c73d1751d8
andre-le <leduyanh1011998@gmail.com>
2017-06-01 11:34:21 +0700

    fix post_controller and minor bug

--- web/controllers/post_controller.ex
+++ web/controllers/post_controller.ex
@@ -7,17 +7,23 @@ defmodule PhoenixBlog.PostController do
   plug :assign_user
 
   def index(conn, _params) do
-    posts = Repo.all(Post)
+    posts = Repo.all(assoc(conn.assigns[:user], :posts))
     render(conn, "index.html", posts: posts)
   end
 
   def new(conn, _params) do
-    changeset = Post.changeset(%Post{})
+    changeset =
+      conn.assigns[:user]
+      |> build_assoc(:posts)
+      |> Post.changeset()
     render(conn, "new.html", changeset: changeset)
   end
 
   def create(conn, %{"post" => post_params}) do
-   changeset = Post.changeset(%Post{}, post_params)
+    changeset =
+      conn.assigns[:user]
+      |> build_assoc(:posts)
+      |> Post.changeset(post_params)
     case Repo.insert(changeset) do
       {:ok, _post} ->
         conn
@@ -29,18 +35,18 @@ defmodule PhoenixBlog.PostController do
   end
 
   def show(conn, %{"id" => id}) do
-    post = Repo.get!(Post, id)
+    post = Repo.get!(assoc(conn.assigns[:user], :posts), id)
     render(conn, "show.html", post: post)
   end
 
   def edit(conn, %{"id" => id}) do
-    post = Repo.get!(Post, id)
+    post = Repo.get!(assoc(conn.assigns[:user], :posts), id)
     changeset = Post.changeset(post)
     render(conn, "edit.html", post: post, changeset: changeset)
   end
 
   def update(conn, %{"id" => id, "post" => post_params}) do
-    post = Repo.get!(Post, id)
+    post = Repo.get!(assoc(conn.assigns[:user], :posts), id)
     changeset = Post.changeset(post, post_params)
     case Repo.update(changeset) do
       {:ok, post} ->
@@ -53,7 +59,7 @@ defmodule PhoenixBlog.PostController do
   end
 
   def delete(conn, %{"id" => id}) do
-    post = Repo.get!(Post, id)
+    post = Repo.get!(assoc(conn.assigns[:user], :posts), id)
     # Here we use delete! (with a bang) because we expect
     # it to always work (and if it does not, it will raise).
     Repo.delete!(post)
@@ -65,10 +71,18 @@ defmodule PhoenixBlog.PostController do
   defp assign_user(conn, _opts) do
     case conn.params do
       %{"user_id" => user_id} ->
-        user = Repo.get(PhoenixBlog.User, user_id)
-        assign(conn, :user, user)
-      _ ->
-        conn
+        case Repo.get(PhoenixBlog.User, user_id) do
+          nil  -> invalid_user(conn)
+          user -> assign(conn, :user, user)
+        end
+      _ -> invalid_user(conn)
     end
+  end
+
+  defp invalid_user(conn) do
+    conn
+    |> put_flash(:error, "Invalid user!")
+    |> redirect(to: page_path(conn, :index))
+    |> halt
   end
 end
